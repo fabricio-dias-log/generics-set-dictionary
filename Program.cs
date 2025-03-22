c1e7694d32c54c11ff947a85c1b61451d41ae070
Fabricio Dias <fabricio270909@gmail.com>
2025-03-22 11:44:03 -0300

    feat: Program flow without generics

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
-﻿namespace GenericsSetDictionary;
+﻿using GenericsSetDictionary.Entities;
+
+namespace GenericsSetDictionary;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        PrintService printService = new PrintService();
+        
+        Console.Write("How many values? ");
+        int qty = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < qty; i++) {
+            int value = int.Parse(Console.ReadLine());
+            printService.AddValue(value);
+        }
+        
+        printService.Print();
+        Console.WriteLine($"First: {printService.First()}");
     }
 }
