4f7ccc9646279dbc1e8802d869a75525155f3650
Fabricio Dias <fabricio270909@gmail.com>
2025-03-22 13:00:16 -0300

    feat: Using generics in methods

--- Entities/CalculationService.cs
+++ Entities/CalculationService.cs
@@ -18,14 +18,16 @@ public class CalculationService {
         Console.WriteLine($"Max: {max}");
     }
     
-    public int Max(List<int> list) {
+    // não é possivel comparar classes genericas, então necessario atribuir
+    // IComparable para T para usar CompareTo()
+    public T Max<T>(List<T> list) where T : IComparable { 
         if (list.Count == 0) {
             throw new ArgumentException("The list can't be empty");
         }
 
-        int max = list[0];
+        T max = list[0];
         for (int i = 1; i < list.Count; i++) {
-            if (list[i] > max) {
+            if (list[i].CompareTo(max) > 0) {
                 max = list[i];
             }
         }
