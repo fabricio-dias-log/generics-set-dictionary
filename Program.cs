7d955b4dbabd4433e0d3b047a29302ef1c7a03d1
Fabricio Dias <fabricio270909@gmail.com>
2025-03-22 12:42:05 -0300

    feat: Simple menu

--- Program.cs
+++ Program.cs
@@ -4,10 +4,32 @@ namespace GenericsSetDictionary;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        PrintService<int> printService = new PrintService<int>();
-        printService.Init(printService);
-        
+    static void Main(string[] args) {
+        int option;
+
+        do {
+            Console.WriteLine("----------MENU----------");
+            Console.WriteLine("1 - Print Service");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("2 - Calculation Service");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("0 - Exit");
+            Console.Write("Select an option:");
+            option = int.Parse(Console.ReadLine() ?? "0");
+            Console.Clear();
+            switch (option) {
+                case 1: {
+                    PrintService<int> printService = new PrintService<int>();
+                    printService.Init(printService);
+                    break;
+                }
+
+                case 0:
+                    break;
+                default:
+                    Console.WriteLine("Invalid option!");
+                    break;
+            }
+        } while (option != 0);
     }
 }
