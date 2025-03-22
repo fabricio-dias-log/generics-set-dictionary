97db2120dbea535547c6e69a50ed7d2093932ecd
Fabricio Dias <fabricio270909@gmail.com>
2025-03-22 13:12:09 -0300

    feat: Implementing IComparable

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -1,6 +1,6 @@
 namespace GenericsSetDictionary.Entities;
 
-public class Product {
+public class Product : IComparable{
     public string Name { get; set; }
     public double Price { get; set; }
 
@@ -11,5 +11,15 @@ public class Product {
 
     public override string ToString() {
         return $"Name: {Name}; Price: {Price}";
+    }
+
+    public int CompareTo(object? obj) {
+        if (!(obj is Product)) {
+            throw new ArgumentException("Comparing error: Argument is not a Product");
+        }
+        
+        Product other = obj as Product;
+        
+        return Price.CompareTo(other.Price);
     }
 }
